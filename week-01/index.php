e0b3377cc8779abc63d84175af39cb206c024aaf
DevAhmedKhalil <dev.akhalil01@gmail.com>
2024-02-11 00:48:27 +0200

    php week-01

---
+++ week-01/index.php
@@ -0,0 +1,55 @@
+<?= '-------------------- WEEK-01 --------------------------' ?>
+<?= '-------------------- التكليف 01 --------------------------' ?>
+<?= "</br>" ?>
+<?php
+// Week 1 Assignments
+#  Week 1 Assignments
+/* Love PHP */
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?= "My First PHP"; ?></title>
+</head>
+
+<body>
+  <div><?php echo "We Love"; ?></div>
+  <div><?php print "Elzero Channel"; ?></div>
+</body>
+
+</html>
+
+<!-- <?= '-------------------- التكليف 02 --------------------------' ?> -->
+
+<?php
+
+// echo 'Prevent Me From Running Please';
+#  echo 'Prevent Me From Running Please';
+/* echo 'Prevent Me From Running Please';*/
+
+?>
+
+<!-- <?= '-------------------- التكليف 03 --------------------------' ?> -->
+
+<?php
+/* 
+  My Application
+  Version 1.0
+  Created By Elzero
+*/
+?>
+
+<!-- <?= '-------------------- التكليف 04 --------------------------' ?> -->
+<?php
+// ## First Comment
+# // # Second Comment
+/* /* /* Third Comment */
+////// Fourth Comment
+// All Comments Are Right
+?>
